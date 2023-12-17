7addca224197cf199033e914ebebd85862ce5dc9
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-12-17 13:44:34 +0530

    add homepage

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,21 @@
+
+
+export default function Homepage(){
+    return (
+        <div>
+            {/* Page Header */}
+            <header>
+                <h1 style={{ textAlign: 'center'}}>Header</h1>
+            </header>
+
+            <content>
+
+            </content>
+
+            {/* Page Footer */}
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
